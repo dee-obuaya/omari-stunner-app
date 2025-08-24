945a1c57b1e1e8ad51216df44bb30c8333896277
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-08-24 16:52:12 +0100

    update rename class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 export default function Footer() {
     return (
-        <div class='fixed bottom-0 left-0 w-full'>
+        <div className='fixed bottom-0 left-0 w-full'>
             <footer className='footer footer-horizontal footer-center text-neutral p-4 justify-self-end'>
                 <aside>
                     <p>Copyright © {new Date().getFullYear()} - Omari Stunner</p>
