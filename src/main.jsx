39e16c379c1030940504cdc1f7f6d77b24f26f11
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-08-24 16:27:58 +0100

    update: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import MainApp from './routes'
+import ErrorPage from './error'
+import Services from './routes/Service'
+import Home from './routes/Home'
+import Gallery from './routes/Gallery'
+import Contact from './routes/Contact'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainApp />,
+    errorElement: <ErrorPage />,
+    children: [
+        {
+            path: '/home',
+            element: <Home />,
+        },
+        {
+            path: '/gallery',
+            element: <Gallery />,
+        },
+        {
+            path: '/services',
+            element: <Services />,
+        },
+        {
+            path: '/contact',
+            element: <Contact />,
+        }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
