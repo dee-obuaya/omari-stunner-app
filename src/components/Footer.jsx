12d7761f6bb8ed7d376ecc3c1f9b2d8741c3e745
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-08-27 17:56:06 +0100

    update styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <div className='fixed bottom-0 left-0 w-full'>
-            <footer className='footer footer-horizontal footer-center text-neutral p-4 justify-self-end'>
+        <div className='bottom-0 left-0 w-full'>
+            <footer className='footer footer-horizontal footer-center text-neutral p-4 justify-self-end text-xs'>
                 <aside>
                     <p>Copyright © {new Date().getFullYear()} - Omari Stunner</p>
                 </aside>
