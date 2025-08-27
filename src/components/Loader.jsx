bbebad2acabc3c4d67428323a1bce8e4b40fd9ca
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-08-27 17:56:21 +0100

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,25 @@
+export default function Loader (props) {
+    const { size, tip } = props;
+
+    const getLoader = loaderSize => {
+        switch (loaderSize) {
+            case 'sm':
+                return <span className="loading loading-ring loading-sm"></span>;
+            case 'md':
+                return <span className="loading loading-ring loading-md"></span>;
+            case 'lg':
+                return <span className="loading loading-ring loading-lg"></span>;
+            case 'xl':
+                return <span className="loading loading-ring loading-xl"></span>;
+            default:
+                return <span className="loading loading-ring loading-xs"></span>;
+        };
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center justify-self-center h-96 space-y-0.5">
+            {getLoader(size)}
+            <p className="text-base text-base-content font-niconne tracking-wider">{tip || 'Loading...'}</p>
+        </div>
+    );
+}
