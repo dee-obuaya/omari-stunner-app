f6a609ea9dba524a2fb5bcf0f8008929158ef814
Chukwudumebi Obuaya <vannyglam@gmail.com>
2025-11-15 19:29:35 +0100

    update: replace url with serverurl constant, include conditional for if tabs are present and display those that are set to active

--- src/components/NavTab.jsx
+++ src/components/NavTab.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { API_BASE_URL } from '../constants/ServerUrl';
 
 export default function NavTab(props) {
     const {activeTab, onTabChange} = props;
@@ -12,9 +13,15 @@ export default function NavTab(props) {
 
     const getTabs = async() => {
         try {
-            const response = await fetch('http://localhost:5000/api/tabs');
+            const response = await fetch(`${API_BASE_URL}/api/tabs`, {credentials: 'include'});
             const data = await response.json();
-            setTabs(data);
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Failed to fetch tabs');
+            } else {
+                setTabs(data.tabs);
+            }
+            // create a check for if data is empty or returns an error
         } catch (e) {
             console.error('Error fetching tabs:', e);
         };
@@ -22,21 +29,24 @@ export default function NavTab(props) {
 
     return (
         <>
-            <div role="tablist" className="tabs tabs-lift tabs-sm md:tabs-lg lg:tabs-xl justify-center md:w-full font-italiana tracking-widest">
-                {/* <span class="grow border-b border-base-300"></span> */}
-                {tabs.map((tab, e) => (
-                    <Link
-                        to={'/' + tab.name}
-                        key={tab.name}
-                        role="tab"
-                        className={`tab${(tab.name === activeTab) ? ' tab-active' : ''} ${tab.name}-tab`}
-                        onClick={() => onTabChange(tab.name)}
-                    >
-                        {tab.label}
-                    </Link>
-                ))}
-                {/* <span class="grow border-b border-base-300"></span> */}
-            </div>
+            {tabs && (
+                <div role="tablist" className="tabs tabs-lift tabs-sm md:tabs-lg lg:tabs-xl justify-center md:w-full font-italiana tracking-widest">
+                    {/* <span class="grow border-b border-base-300"></span> */}
+                    {tabs.map((tab, e) => (
+                        tab.active &&
+                        <Link
+                            to={'/' + tab.name}
+                            key={tab.name}
+                            role="tab"
+                            className={`tab${(tab.name === activeTab) ? ' tab-active' : ''} ${tab.name}-tab`}
+                            onClick={() => onTabChange(tab.name)}
+                        >
+                            {tab.label}
+                        </Link>
+                    ))}
+                    {/* <span class="grow border-b border-base-300"></span> */}
+                </div>
+            )}
         </>
     );
 }
